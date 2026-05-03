2913e1e35b0f0ebf40a10975d85c5ad421589b92
toast333 <b13701156@ntu.edu.tw>
2026-05-03 19:30:35 +0800

    Initial Commit

---
+++ eatoast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eatoast
+//
+//  Created by 114-2Workshop13 on 2026/5/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eatoast/eatoastApp.swift
@@ -0,0 +1,17 @@
+//
+//  eatoastApp.swift
+//  eatoast
+//
+//  Created by 114-2Workshop13 on 2026/5/3.
+//
+
+import SwiftUI
+
+@main
+struct eatoastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
